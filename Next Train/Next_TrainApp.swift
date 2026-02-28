7bc195549b94e3241e48bfc46405e99a03c6dbc6
Linus Unghammar <unghammarlinus@gmail.com>
2026-02-28 12:25:27 +0100

    Initial Commit

---
+++ Next Train/Next_TrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Next_TrainApp.swift
+//  Next Train
+//
+//  Created by Linus Unghammar on 2026-02-28.
+//
+
+import SwiftUI
+
+@main
+struct Next_TrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Next TrainTests/Next_TrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  Next_TrainTests.swift
+//  Next TrainTests
+//
+//  Created by Linus Unghammar on 2026-02-28.
+//
+
+import Testing
+@testable import Next_Train
+
+struct Next_TrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
